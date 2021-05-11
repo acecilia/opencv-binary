978ae448d49bffb7e3fc09e826d51bea1d56d6f3
Andres Cecilia Luque <andres.luque@revolut.com>
2021-05-11 12:11:02 +0100

    Support building opencv 4.5.2

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
-opencv_xcframework:
-	Scripts/opencv_xcframework.sh
+opencv_3_4_6:
+	Scripts/opencv_xcframework.sh build_3_4_6
 
+opencv_4_5_2:
+	Scripts/opencv_xcframework.sh build_4_5_2
--- scripts/opencv_xcframework.sh
+++ scripts/opencv_xcframework.sh
@@ -1,23 +1,42 @@
 #!/bin/zsh
 
+set -euo pipefail
+
 readonly BUILD_DIR="opencv_build"
-readonly OPENCV_452_DIR="${BUILD_DIR}/4.5.2"
-readonly OPENCV_346_DIR="${BUILD_DIR}/3.4.6"
 readonly OUTPUT_DIR="${BUILD_DIR}/output"
 
+clone() {
+    readonly VERSION="${1}"
+    git clone --branch "${VERSION}" --depth 1 https://github.com/opencv/opencv.git "${BUILD_DIR}/${VERSION}"
+}
+
+build_xcframework() {
+    readonly VERSION="${1}"
+    shift
+
+    python3 "${BUILD_DIR}/${VERSION}/platforms/apple/build_xcframework.py" \
+        --build_only_specified_archs \
+        --iphoneos_archs arm64,armv7 \
+        --iphonesimulator_archs arm64,x86_64 \
+        --out "${OUTPUT_DIR}" \
+        "$@"
+}
+
+build_3_4_6() {
+    clone 3.4.6
+    clone 4.5.2
+
+    # Add xcframework support to opencv 3.4.6
+    rm -rf "${BUILD_DIR}/3.4.6/platforms"
+    cp -rf "${BUILD_DIR}/4.5.2/platforms" "${BUILD_DIR}/3.4.6/platforms"
+
+    build_xcframework 3.4.6 --legacy_build
+}
+
+build_4_5_2() {
+    clone 4.5.2
+    build_xcframework 4.5.2
+}
+
 rm -rf "${BUILD_DIR}"
-
-git clone --branch 4.5.2 --depth 1 https://github.com/opencv/opencv.git "${OPENCV_452_DIR}"
-git clone --branch 3.4.6 --depth 1 https://github.com/opencv/opencv.git "${OPENCV_346_DIR}"
-
-# Add xcframework support to opencv 3.4.6
-rm -rf "${OPENCV_346_DIR}/platforms"
-cp -rf "${OPENCV_452_DIR}/platforms" "${OPENCV_346_DIR}/platforms"
-
-python3 "${OPENCV_346_DIR}/platforms/apple/build_xcframework.py" \
-    --build_only_specified_archs \
-    --iphoneos_archs arm64,armv7 \
-    --iphonesimulator_archs arm64,x86_64 \
-    --legacy_build \
-    --out "${OUTPUT_DIR}"
-
+"$@"
